2f3ad6583b9a93fc221096e0a13ab3f66dacae33
Shai Wilson <sjwilson2@usfca.edu>
2016-04-13 20:50:21 -0700

    work on parsing command line arguments

--- sugar-cl/sugar.py
+++ sugar-cl/sugar.py
@@ -27,6 +27,19 @@ def connect_to_db(app):
 
 # db control
 
+def make_new_user(first_name, last_name, github):
+    """Add a new user and print confirmation.
+
+    Given a first name, last name, and GitHub account, add user to the
+    database and print a confirmation message.
+    """
+
+    QUERY = """INSERT INTO Students VALUES (:first_name, :last_name, :github)"""
+    db_cursor = db.session.execute(QUERY, {'first_name': first_name, 'last_name': last_name, 'github': github})
+    db.session.commit()
+
+    print "Hi %s %s, you're setup to start using sugar! :-)" % (first_name, last_name)
+
 def punch_in():
    """Given the start command, log timestamp information for the current work session."""
 
@@ -44,6 +57,43 @@ def clear_intervals():
 
     # clear db
     print "Cleared all intervals"
+
+def get_all_hours():
+    """Get the sum of all hours"""
+
+    QUERY = """
+        
+        """
+    db_cursor = db.session.execute(QUERY)
+    rows = db_cursor.fetchall()
+    return rows
+
+
+def handle_input(args):
+    """Helper function to direct commands to corresponding actions."""
+
+    command = args
+
+    if command == "setup":
+        first_name = raw_input("What is your username?")
+        last_name = raw_input("What is your last name?")
+        github = raw_input("What is your github username?")
+        make_new_student(first_name, last_name, github)
+
+
+    if command == "start":
+        punch_in()
+
+    elif command == "stop":
+        punch_out()
+
+    elif command == "show":
+        show_intervals()
+
+    elif command == "clear":
+        clear_intervals()
+
+
 
 def main():
 
@@ -72,6 +122,7 @@ def main():
 
             Commands:
 
+                sugar.py setup   add username and github account
                 sugar.py start   start the clock
                 sugar.py stop    stop the clock
                 sugar.py show    display all saved intervals
@@ -89,24 +140,6 @@ def main():
     # show usage text for commands without args
     if len(args) != 1:
         parser.print_help()
-
-def handle_input(args):
-    """Helper function to direct commands to corresponding actions."""
-
-    command = args
-
-    if command == "start":
-        punch_in()
-
-    elif command == "stop":
-        punch_out()
-
-    elif command == "show":
-        show_intervals()
-
-    elif command == "clear":
-        clear_intervals()
-
 
 
 if __name__ == '__main__':
