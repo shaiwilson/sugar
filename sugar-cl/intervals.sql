47927dcf812d0e40c006f6fef5fcb12da9bd9dd2
Shai Wilson <sjwilson2@usfca.edu>
2016-04-16 13:54:41 -0700

    refactor db

--- sugar-cl/intervals.sql
+++ sugar-cl/intervals.sql
@@ -27,6 +27,96 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: intervals; Type: TABLE; Schema: public; Owner: shai
+--
+
+CREATE TABLE intervals (
+    id integer NOT NULL,
+    date date,
+    start_time time without time zone,
+    end_time time without time zone
+);
+
+
+ALTER TABLE intervals OWNER TO shai;
+
+--
+-- Name: intervals_id_seq; Type: SEQUENCE; Schema: public; Owner: shai
+--
+
+CREATE SEQUENCE intervals_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE intervals_id_seq OWNER TO shai;
+
+--
+-- Name: intervals_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: shai
+--
+
+ALTER SEQUENCE intervals_id_seq OWNED BY intervals.id;
+
+
+--
+-- Name: students; Type: TABLE; Schema: public; Owner: shai
+--
+
+CREATE TABLE students (
+    username character varying(30)
+);
+
+
+ALTER TABLE students OWNER TO shai;
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: shai
+--
+
+ALTER TABLE ONLY intervals ALTER COLUMN id SET DEFAULT nextval('intervals_id_seq'::regclass);
+
+
+--
+-- Data for Name: intervals; Type: TABLE DATA; Schema: public; Owner: shai
+--
+
+COPY intervals (id, date, start_time, end_time) FROM stdin;
+\.
+
+
+--
+-- Name: intervals_id_seq; Type: SEQUENCE SET; Schema: public; Owner: shai
+--
+
+SELECT pg_catalog.setval('intervals_id_seq', 1, false);
+
+
+--
+-- Data for Name: students; Type: TABLE DATA; Schema: public; Owner: shai
+--
+
+COPY students (username) FROM stdin;
+\.
+
+
+--
+-- Name: intervals_pkey; Type: CONSTRAINT; Schema: public; Owner: shai
+--
+
+ALTER TABLE ONLY intervals
+    ADD CONSTRAINT intervals_pkey PRIMARY KEY (id);
+
+
 --
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
 --
