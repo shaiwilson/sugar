e43e9c24959d7e65c6e7628b13ac9cb657128a5c
Shai Wilson <sjwilson2@usfca.edu>
2016-04-16 12:26:55 -0700

    fix cmd line args

--- sugar-cl/sugar.py
+++ sugar-cl/sugar.py
@@ -52,96 +52,108 @@ def make_new_user(first_name, last_name, github):
     print "Hi %s %s, you're setup to start using sugar! :-)" % (first_name, last_name)
 
 def punch_in():
-   """Given the start command, log timestamp information for the current work session."""
+  """Given the start command, log timestamp information for the current work session."""
 
-   # first make sure they did not already punch in 
+  # first make sure they did not already punch in 
 
-   now = datetime.now()
-   current_time = now.strftime(TIME_DB_FORMAT)
-   current_date = now.strftime(DATE_DB_FORMAT)
-   print_format = now.strftime(TIME_PRINT_FORMAT)
+  now = datetime.now()
+  current_time = now.strftime(TIME_DB_FORMAT)
+  current_date = now.strftime(DATE_DB_FORMAT)
+  print_format = now.strftime(TIME_PRINT_FORMAT)
 
-   # change "date " in intervals db to "date_time"
-   QUERY = """INSERT INTO Intervals (date_time, start_time)
-               VALUES (:date_time, :start_time)"""
+  # change "date " in intervals db to "date_time"
+  QUERY = """INSERT INTO Intervals (date_time, start_time)
+           VALUES (:date_time, :start_time)"""
 
-   db_cursor = db.session.execute(QUERY, {'date_time': current_date, 'start_time': current_time})
+  db_cursor = db.session.execute(QUERY, {'date_time': current_date, 'start_time': current_time})
 
-   print "Punched in at %s" %(print_format)
+  print "Punched in at %s" %(print_format)
 
 
 def punch_out():
-    """Given the stop command, log timestamp information for the current work session."""
+  """Given the stop command, log timestamp information for the current work session."""
 
-   now = datetime.now()
-   current_time = now.strftime(TIME_DB_FORMAT)
-   current_date = now.strftime(DATE_DB_FORMAT)
-   print_format = now.strftime(TIME_PRINT_FORMAT)
+  now = datetime.now()
+  current_time = now.strftime(TIME_DB_FORMAT)
+  current_date = now.strftime(DATE_DB_FORMAT)
+  print_format = now.strftime(TIME_PRINT_FORMAT)
 
-   # change "date " in intervals db to "date_time"
-   QUERY = """INSERT INTO Intervals (date_time, end_time)
-               VALUES (:date_time, :end_time)"""
+  # change "date " in intervals db to "date_time"
+  # QUERY = """INSERT INTO Intervals (date_time, end_time)
+  #             VALUES (:date_time, :end_time)"""
 
-   db_cursor = db.session.execute(QUERY, {'date_time': current_date, 'end_time': current_time})
+  # db_cursor = db.session.execute(QUERY, {'date_time': current_date, 'end_time': current_time})
 
-   print "Punched in at %s" %(print_format)
+  print "Punched in at %s" %(print_format)
 
 def show_intervals(timeframe):
-    """Given the show command, display daily or weekly information for the current work session."""
-    if timeframe == 1:
+  """Given the show command, display daily or weekly information for the current work session."""
+    # if timeframe == 1:
             
 
-    elif timeframe == 2:
+    # elif timeframe == 2:
             
 
 def clear_intervals():
-    """Given the clear command, clear log information of work sessions."""
+  """Given the clear command, clear log information of work sessions."""
 
-    # clear db
-    print "Cleared all intervals"
+  # clear db print "Cleared all intervals"
 
 def get_all_hours():
-    """Get the sum of all hours"""
+  """Get the sum of all hours"""
 
-    QUERY = """
-        
-        """
-    db_cursor = db.session.execute(QUERY)
-    rows = db_cursor.fetchall()
-    return rows
+  QUERY = """
+      
+      """
+  db_cursor = db.session.execute(QUERY)
+  rows = db_cursor.fetchall()
+  return rows
 
 
 def handle_input(args):
-    """Helper function to direct commands to corresponding actions."""
+  """Helper function to direct commands to corresponding actions."""
 
-    command = args
+  command = args
 
-    if command == "setup":
-        first_name = raw_input("What is your username?")
-        last_name = raw_input("What is your last name?")
-        github = raw_input("What is your github username?")
-        make_new_student(first_name, last_name, github)
+  print "handle input"
+
+  if command == "setup":
+      first_name = raw_input("What is your username?")
+      last_name = raw_input("What is your last name?")
+      github = raw_input("What is your github username?")
+      make_new_student(first_name, last_name, github)
 
 
-    if command == "start":
-        punch_in()
+  if command == "start":
+      punch_in()
 
-    elif command == "stop":
-        punch_out()
+  elif command == "stop":
+      punch_out()
 
-    elif command == "show":
-        timeframe = raw_input("enter 1 to see daily sum or 2 to see weekly sum")
-        show_intervals(timeframe)
+  elif command == "show":
+      timeframe = raw_input("enter 1 to see daily sum or 2 to see weekly sum")
+      show_intervals(timeframe)
 
-    elif command == "clear":
-        clear_intervals()
+  elif command == "clear":
+      clear_intervals()
 
 
 
 def main():
 
+    # handle input
     parser = argparse.ArgumentParser(description='Log the amount of hours you code per day.')
+    args = parser.parse_args()
+    subparsers = parser.add_subparsers()
 
+
+    # show usage text for commands without args
+    # if len(args) != 1:
+    #     parser.print_help()
+
+    # handle_input(args)
+
+    setup_parser = subparsers
     parser.add_argument('setup',
                    help='Give your username, first name, and github to sugar')
 
@@ -180,9 +192,7 @@ def main():
 
     # options, arguments = parser.parse_args()
 
-    # show usage text for commands without args
-    if len(args) != 1:
-        parser.print_help()
+    
 
 
 if __name__ == '__main__':
@@ -190,9 +200,7 @@ if __name__ == '__main__':
     app = Flask(__name__)
     connect_to_db(app)
 
-    # handle input
-    args = parser.parse_args()
-    handle_input(args)
+    
 
     db.session.close()
 
